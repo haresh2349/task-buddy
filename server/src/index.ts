7603423a2d6e584f2e6190113601ca0bddec4ce1
haresh2349 <hareshcsolanki07@gmail.com>
2025-05-17 00:48:29 +0530

    update cors allowed origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,12 @@ import { errorHandler } from "./middlewares/error-handler-middleware";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
